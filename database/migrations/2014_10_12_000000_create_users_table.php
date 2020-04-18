1032af77660e94c3fcb7669786acc26f24110df7
Tuyen nh <tuyen.nh@deha-soft.com>
2020-04-18 22:12:20 +0700

    add commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('phone');
+            $table->string('avatar')->default('img_avatar2.png');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
